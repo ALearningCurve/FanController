a0e379c52f5235dd4e8f0b855efa3a5f5cb7722e
Will Walling-Sotolongo <walling-sotolongo.w@northeastern.edu>
2023-04-08 17:52:09 -0400

    fix cmake

--- FanControllerFirmware/build.sh
+++ FanControllerFirmware/build.sh
@@ -8,6 +8,7 @@ fi
 
 # Get directory path of this script
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo $SCRIPT_DIR
 SHARED_LIB_DIR=$HOME/rover/firmware/lib
 
 # Build project
@@ -20,4 +21,4 @@ docker run \
     --mount=type=bind,source=$SHARED_LIB_DIR,destination=$SHARED_LIB_DIR \
     -w $SCRIPT_DIR \
     ghcr.io/armmbed/mbed-os-env:mbed-os-6-latest \
-    bash -c "mkdir -p build/${profile} && cd build/${profile} && cmake -D CMAKE_BUILD_TYPE=${profile} ../.. && make -j$(nproc)"
+    bash -c "mkdir -p build/${profile} && cd build/${profile} && cmake -D CMAKE_BUILD_TYPE=${profile} ../.. && echo $PWD && make -j$(nproc)"
